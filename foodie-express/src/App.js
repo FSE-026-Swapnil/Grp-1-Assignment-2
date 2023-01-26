3f821a8bb4ef8cb8eed0005680a743c72495ae88
Swapnil Khande <swapnilkhande6996@gmail.com>
2023-01-26 17:16:10 +0530

    Done with authentication using json-server

--- foodie-express/src/App.js
+++ foodie-express/src/App.js
@@ -1,25 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './App.css'
-import Header from './Components/Layout/Header/Header';
-import Footer from './Components/Layout/Footer/Footer';
-import Routers from './Routers/Routers';
-
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./App.css";
+import Header from "./Components/Layout/Header/Header";
+import Footer from "./Components/Layout/Footer/Footer";
+import Routers from "./Routers/Routers";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div class="container-fluid p-0">
-      <BrowserRouter >
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <BrowserRouter>
         <Header />
-        <Routers/>  
+        <Routers />
         <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- foodie-express/src/Components/Login/Login.js
+++ foodie-express/src/Components/Login/Login.js
@@ -1,31 +1,105 @@
-import react, { useState } from "react";
+import react, { useState, useRef } from "react";
 import "./Login.css";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import { BASE_URL } from '../../Constants/APIConstants'
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isOwner, setIsOwner] = useState(false);
   const [isAgent, setIsAgent] = useState(false);
+  const [customerType, setCustomerType] = useState('');
+  const emailRef = useRef('');
+  const passRef = useRef('');
+  const nameRef = useRef('');
+  const fssaiRef = useRef('');
+  const addrRef = useRef('');
+  const mobRef = useRef('');
 
   const registerNow = () => {
     setIsLogin(false);
     setIsOwner(false);
     setIsAgent(false);
+    emailRef.current.value ='';
+    passRef.current.value = '';
   };
 
   const loginNow = () => {
     setIsLogin(true);
+    emailRef.current.value ='';
+    passRef.current.value = '';
   };
 
   const optionHandler = (event) => {
     if (event.target.value === "Restaurant Owner") {
       setIsAgent(false);
       setIsOwner(true);
+      setCustomerType('Restaurant Owner');
     } else if (event.target.value === "System User") {
       setIsOwner(false);
       setIsAgent(true);
+      setCustomerType('System User');
     } else {
       setIsOwner(false);
       setIsAgent(false);
+      setCustomerType('Customer');
+    }
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    let userData = {
+      email : emailRef.current.value,
+      password : passRef.current.value
+    };
+    
+    if(customerType === 'Restaurant Owner'){
+      userData = {
+        ...userData,
+        restaurantName : nameRef.current.value,
+        restaurantAddr : addrRef.current.value,
+        fssaiNo : fssaiRef.current.value
+      }
+    }else if(customerType === 'System User'){
+      userData = {
+        ...userData,
+        mobileNo : mobRef.current.value
+      }
+    }
+
+    console.log("userData ==>",userData,BASE_URL);
+
+    if(isLogin){
+      // Login
+      fetch(BASE_URL + 'users?email=' + emailRef.current.value, {
+        method: "GET"
+      }).then((response) => {
+        return response.json();
+      })
+      .then((resp) => {
+        console.log(resp);
+        if(resp.length > 0){
+          if(resp[0].password === passRef.current.value){
+            toast.success("Logged in successfully.");
+          }else{
+            toast.error("Invalid credentials.");
+          }
+        }else{
+          toast.warn("Please enter valid credentials.");
+        }
+      });
+    }else{
+      // Sign Up
+      fetch(BASE_URL + 'users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+      }).then((response) => {
+        toast.success('Hurray!!! Registration completed.');
+      });
     }
   };
 
@@ -35,13 +109,14 @@ const Login = () => {
         {isLogin ? <h1>Sign In</h1> : <h1>Register</h1>}
       </div>
       <div className="card-body">
-        <form>
+        <form onSubmit={submitHandler}>
           <div className="mb-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control"
               placeholder="Enter email"
+              ref={emailRef}
             />
           </div>
 
@@ -51,6 +126,7 @@ const Login = () => {
               type="password"
               className="form-control"
               placeholder="Enter password"
+              ref={passRef}
             />
           </div>
 
@@ -74,6 +150,7 @@ const Login = () => {
                   type="text"
                   className="form-control"
                   placeholder="Enter restaurant name"
+                  ref={nameRef}
                 />
               </div>
 
@@ -83,6 +160,7 @@ const Login = () => {
                   type="text"
                   className="form-control"
                   placeholder="Enter restaurant address"
+                  ref={addrRef}
                 />
               </div>
 
@@ -92,26 +170,19 @@ const Login = () => {
                   type="number"
                   className="form-control"
                   placeholder="Enter FSSAI no."
+                  ref={fssaiRef}
                 />
               </div>
             </div>
           ) : isAgent && !isLogin ? (
             <div>
               <div className="mb-3">
-                <label>Home Address</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter home address"
-                />
-              </div>
-
-              <div className="mb-3">
                 <label>Mobile No.</label>
                 <input
                   type="number"
                   className="form-control"
                   placeholder="Enter mobile no."
+                  ref={mobRef}
                 />
               </div>
             </div>
