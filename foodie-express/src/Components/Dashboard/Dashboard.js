9b3ecee2e2d6c2b410c8fb9d2d70a0db0fc1df38
Swapnil Khande <swapnilkhande6996@gmail.com>
2023-02-05 00:02:32 +0530

    Made dashboard option active

--- foodie-express/src/Components/Dashboard/Dashboard.js
+++ foodie-express/src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {BrowserRouter,Route,Routes, Link} from 'react-router-dom';
 import './Dashboard.css'
 import { DashboardData } from './DashboardData';
 import { IconContext } from 'react-icons';
 
 function Dashboard() {
+  const [active, setActive] = useState(null);
   return (
     <div className='dasboard-container'>
         <IconContext.Provider value={{ color: '#fff' }}>
@@ -12,7 +13,7 @@ function Dashboard() {
                 <ul className='nav-menu-items'>
                     {DashboardData.map((item, index) => {
                     return (
-                        <li key={index} className={item.cName}>
+                        <li key={index} className={`nav-text ${active == item && 'selected'}`} onClick={() => setActive(item)}>
                         <Link to={item.path}>
                             {item.icon}
                             <span>{item.title}</span>
